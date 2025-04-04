eae8c28fcf78245053480bdc6042eec6c4afbccf
KetutDana <ketutdana@KetutDanas-MacBook-Pro.local>
2025-04-04 17:17:03 +0800

    update service provider

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,9 @@ use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\File;
 
 Route::get('visitor-log', function () {
-    // Ambil path dari config
     $filePath = config('visitor-tracker.storage_path', storage_path('app/visitor.json'));
-
-    // Baca file JSON jika ada, jika tidak buat array kosong
     $visitors = File::exists($filePath) ? json_decode(File::get($filePath), true) : [];
 
-    return view('xblade-visitor-tracker::visitor', compact('visitors'));
+    return view('visitor-tracker::visitor', compact('visitors'));
 })->name('visitor.log');
+
